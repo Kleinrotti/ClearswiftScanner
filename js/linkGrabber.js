6030496ff7db25a361088c0be8c08adf064a7ed9
Kleinrotti <kleinrotti@gmail.com>
2021-07-16 16:24:08 +0200

    improved url detection

--- js/linkGrabber.js
+++ js/linkGrabber.js
@@ -1,14 +1,19 @@
 export function grab() {
     var links = new Array();
+    var unique;
     try {
         var table = document.getElementById("urlDetails");
-        var rows = table.getElementsByClassName("styledTable")[0].tBodies[0].rows;
+        var rows = table.getElementsByClassName("styledTable");
+        for (u = 0; u < rows.length; u++) {
+            var row = rows[u].tBodies[0].rows;
+            for (i = 0; i < row.length; i++) {
+                links.push(row.item(i).innerText);
+            }
+        }
+        //filter doubled entries
+        unique = [...new Set(links)];
     } catch (TypeError) {
         console.debug("no element with urls found")
-        return links;
     }
-    for (i = 0; i < rows.length; i++) {
-        links.push(rows.item(i).innerText);
-    }
-    return links;
+    return unique;
 }
